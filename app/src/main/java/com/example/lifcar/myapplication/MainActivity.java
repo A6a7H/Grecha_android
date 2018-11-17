9ed6d53801bdd32b64ad3a34a2a7786f52e92745
Tornike <tornike99@yandex.ru>
2018-11-17 12:59:08 +0300

    bottom_sheet v1

--- app/src/main/java/com/example/lifcar/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lifcar/myapplication/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.lifcar.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.dagang.library.GradientButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GradientButton chooseTarif;
+    private final String TAG = "MAIN_LOG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // получение вью нижнего экрана
+        LinearLayout llBottomSheet = (LinearLayout) findViewById(R.id.bottom_sheet);
+
+        // настройка поведения нижнего экрана
+        final BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+
+        // настройка состояний нижнего экрана
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        // настройка максимальной высоты
+        bottomSheetBehavior.setPeekHeight(340);
+
+        // настройка возможности скрыть элемент при свайпе вниз
+        bottomSheetBehavior.setHideable(true);
+
+        // настройка колбэков при изменениях
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+        chooseTarif = findViewById(R.id.chooseTarif);
+        chooseTarif.getButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: work!");
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
     }
 }
